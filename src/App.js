bdd99be866fba10bf7f8d6b798bf6895fffa215d
Mackenzie Barrett <mbb640@mun.ca>
2022-03-30 12:56:05 -0230

    DIsplay geolocation fail state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import WeatherDay from "./WeatherDay";
 import Todo from "./Todo";
 const env = require("./env.json");
@@ -7,6 +7,7 @@ const todoList = require("./Todos.json");
 
 function App() {
     const [getState, setState] = useState("");
+    let errorWhenFetchingLocation = useRef(false);
 
     useEffect(() => {
         // use Geolocation to determine user's current location. Default to St. John's, CA
@@ -16,6 +17,7 @@ function App() {
         function success(pos) {
             lat = pos.coords.latitude;
             lon = pos.coords.longitude;
+
             fetch(
                 `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,alerts&APPID=${env.API_KEY}`
             )
@@ -24,15 +26,7 @@ function App() {
         }
 
         function error() {
-            console.log(
-                "Error when determining location. Do you have location enabled?"
-            );
-            // TODO: display this to user
-            fetch(
-                `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,alerts&APPID=${env.API_KEY}`
-            )
-                .then((response) => response.json())
-                .then((response) => setState(response));
+            errorWhenFetchingLocation.current = true;
         }
 
         navigator.geolocation.getCurrentPosition(success, error);
@@ -40,25 +34,36 @@ function App() {
 
     return (
         <div className="App">
-            <div className="days-wrapper">
-                {getState ? (
-                    <div className="weather-forecast">
-                        <div className="day-wrapper">
-                            <WeatherDay current weather={getState.daily[0]} />
+            {errorWhenFetchingLocation.current ? (
+                <div>
+                    Error when determining location. Do you have location
+                    enabled?
+                </div>
+            ) : (
+                <div className="days-wrapper">
+                    {getState ? (
+                        <div className="weather-forecast">
+                            <div className="day-wrapper">
+                                <WeatherDay
+                                    current
+                                    weather={getState.daily[0]}
+                                />
+                            </div>
+                            <div className="day-wrapper">
+                                <WeatherDay weather={getState.daily[1]} />
+                            </div>
+                            <div className="day-wrapper">
+                                <WeatherDay weather={getState.daily[2]} />
+                            </div>
                         </div>
-                        <div className="day-wrapper">
-                            <WeatherDay weather={getState.daily[1]} />
-                        </div>
-                        <div className="day-wrapper">
-                            <WeatherDay weather={getState.daily[2]} />
-                        </div>
-                    </div>
-                ) : (
-                    // had a loading spinner here, but API seems fast enough for loading time to be insignificant.
-                    // suggest react-loading-icons in case of change.
-                    ""
-                )}
-            </div>
+                    ) : (
+                        // had a loading spinner here, but API seems fast enough for loading time to be insignificant.
+                        // suggest react-loading-icons in case of change.
+                        ""
+                    )}
+                </div>
+            )}
+
             <div className="todos-wrapper">
                 {todoList.map((todo) => (
                     <Todo todo={todo} key={todo.description} />
--- src/Todo.js
+++ src/Todo.js
@@ -3,14 +3,14 @@ import "./Todo.css";
 function Todos({ todo }) {
     return (
         <div className="todo">
-            <label htmlFor={todo.description}>
+            <label className="label" htmlFor={todo.description}>
                 <input
                     type="checkbox"
                     id={todo.description}
                     name={todo.description}
                     defaultChecked={todo.completed}
                 />
-                {todo.description}
+                <div>{todo.description}</div>
             </label>
         </div>
     );
